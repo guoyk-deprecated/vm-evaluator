2227189fba278a0f64b4a4b878644d0b5e0ae564
Guo Y.K <hi@guoyk.net>
2021-01-27 17:28:16 +0800

    complete /zero

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+var (
+	optAddr string
+)
+
+type zeroReader struct {
+}
+
+func (z zeroReader) Read(p []byte) (n int, err error) {
+	for i := 0; i < len(p); i++ {
+		p[i] = 0
+	}
+	n = len(p)
+	return
+}
+
+func main() {
+	flag.StringVar(&optAddr, "addr", ":80", "listen address")
+
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/zero", func(c echo.Context) error {
+		return c.Stream(http.StatusOK, "application/octet-stream", zeroReader{})
+	})
+	e.Logger.Fatal(e.Start(optAddr))
+}
